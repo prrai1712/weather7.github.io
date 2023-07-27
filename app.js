66cf854afa34b4f2ea0f62672f70d4657b03c328
priyanshu rai <99195636+prrai1712@users.noreply.github.com>
2023-07-27 14:08:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,60 +1,40 @@
-import React, { useState } from 'react';
-import './styles.css';
+// Replace 'YOUR_API_KEY' with your actual API key from the weather API provider
+const apiKey = '7968a7b78d95e3aecb22bd624dab291d';
+const weatherInfo = document.getElementById('weatherInfo');
+const cityNameElement = document.getElementById('cityName');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+const humidityElement = document.getElementById('humidity');
+const windSpeedElement = document.getElementById('windSpeed');
 
-const apiKey = 'YOUR_API_KEY';
+async function getWeather() {
+    const cityInput = document.getElementById('cityInput').value.trim();
 
-function App() {
-  const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState(null);
-
-  const getWeather = async () => {
-    if (!city) {
-      alert('Please enter a city name.');
-      return;
+    if (!cityInput) {
+        alert('Please enter a city name.');
+        return;
     }
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
 
     try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
+        const response = await fetch(apiUrl);
+        const data = await response.json();
 
-      if (data.cod !== 200) {
-        alert(data.message);
-        return;
-      }
+        if (data.cod !== 200) {
+            alert(data.message);
+            return;
+        }
 
-      setWeatherData(data);
+        cityNameElement.textContent = data.name;
+        temperatureElement.textContent = `Temperature: ${data.main.temp} °C`;
+        descriptionElement.textContent = `Description: ${data.weather[0].description}`;
+        humidityElement.textContent = `Humidity: ${data.main.humidity}%`;
+        windSpeedElement.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+
+        weatherInfo.classList.remove('hidden');
     } catch (error) {
-      alert('An error occurred while fetching weather data.');
-      console.error(error);
+        alert('An error occurred while fetching weather data.');
+        console.error(error);
     }
-  };
-
-  return (
-    <div className="weather-container">
-      <h1>Weather App</h1>
-      <input
-        type="text"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        placeholder="Enter city name"
-      />
-      <button onClick={getWeather}>Get Weather</button>
-
-      {weatherData && (
-        <div id="weatherInfo">
-          <h2 id="cityName">{weatherData.name}</h2>
-          <div id="weatherDetails">
-            <div>Temperature: {weatherData.main.temp} °C</div>
-            <div>Description: {weatherData.weather[0].description}</div>
-            <div>Humidity: {weatherData.main.humidity}%</div>
-            <div>Wind Speed: {weatherData.wind.speed} m/s</div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
 }
-
-export default App;
